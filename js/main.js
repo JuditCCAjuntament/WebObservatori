09f9c2049d83c390f72ce59e96a1c05fc11faace
Judit Closa <judit.closa@AjManresa.loc>
2023-02-17 11:34:06 +0100

    Full menu, search pending

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 //Init materialize sidenav funcrion
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-    console.log("Sidenav Init");
-  });
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  console.log("Init");
+});
+$( ".collapsibleIndicadors" ).hover(
+  function() {
+    $( ".collapsibleItems" ).css("display","flex");
+    console.log("Hovered In Menu");
+  }, function() {
+    console.log("Hovered Out Menu");
+  }
+);
+$( ".collapsibleItems" ).hover(
+  function(){
+    console.log("Hovered In Items");
+  },
+  function() {
+    $( this ).css("display","none");
+    console.log("Hovered out Items");
+  }
+);
