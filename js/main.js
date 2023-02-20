548abb75a68e47b4d899edc2d41920cff1af0dd6
Judit Closa <judit.closa@AjManresa.loc>
2023-02-20 09:49:34 +0100

    Indicadors

--- js/main.js
+++ js/main.js
@@ -5,20 +5,31 @@ $(document).ready(function () {
 });
 
 //Collapsible
-$( ".collapsibleIndicadors" ).hover(
-  function() {
-    $( ".collapsibleItems" ).css("display","flex");
+$(".collapsibleIndicadors").hover(
+  function () {
+    $(".collapsibleItems").css("display", "flex");
     console.log("Hovered In Menu");
-  }, function() {
+  }, function () {
     console.log("Hovered Out Menu");
   }
 );
-$( ".collapsibleItems" ).hover(
-  function(){
+$(".collapsibleItems").hover(
+  function () {
     console.log("Hovered In Items");
   },
-  function() {
-    $( this ).css("display","none");
+  function () {
+    $(this).css("display", "none");
     console.log("Hovered out Items");
+  }
+);
+
+//Anchors
+$(".socioeAnchor").hover(
+  function () {
+    $(this).append($("<i class='material-icons'> chevron_right</i>"));
+    $(this).children("i").css("display","none");
+    $(this).children("i").fadeIn();
+  }, function () {
+    $(this).find("i").last().remove();
   }
 );
